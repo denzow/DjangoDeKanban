5cfb3fc0fa999c274593efef8f11f05ea43795fa
denzow <denzow@gmail.com>
2018-08-19 22:29:14 +0900

    [add] vue-router vuex and modify urls.py

--- application/vuejs/src/main.js
+++ application/vuejs/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app');
+  router,
+  store,
+  el: '#app'
+});
