859d0dfd6efb4c2419a22167cc567ef6b9be9205
denzow <denzow@gmail.com>
2018-09-16 23:10:51 +0900

    [add] daphne mode

--- docker/service/start-server.sh
+++ docker/service/start-server.sh
@@ -3,4 +3,10 @@
 pip install -r requirements.txt
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:3000 --nostatic
+if [ ${DJANGO_ENV} = 'production' ]; then
+    daphne --root-path /app -b 0.0.0.0 -p 3000 application.asgi:application
+else
+    python manage.py runserver 0.0.0.0:3000 --nostatic
+
+fi
+
